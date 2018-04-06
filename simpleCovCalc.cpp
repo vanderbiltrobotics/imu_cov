75b943224af22f6f160881a4ff1cc14071df110d
Josh Petrin <joshua.m.petrin@vanderbilt.edu>
2018-04-06 01:21:16 +0000

    Fixed some bugs in simpleCovCalc.cpp

--- simpleCovCalc.cpp
+++ simpleCovCalc.cpp
@@ -9,11 +9,14 @@ extern "C" {
 #include <string>
 #include "FreeSixIMU.h"
 #include <unistd.h>
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
 typedef double covtype;
 // How big you want the covariance matrix (e.g. how many variables you keep track of)
 #define DIMENSION   3
 
-double[][] covCalculator() {
+void covCalculator(double inputMat[3][9]) {
     // start roboticscape
     rc_initialize();
     // start i2c with the target register pointed to nothing
@@ -53,7 +56,7 @@ double[][] covCalculator() {
         ang_vel_double[1] = (double)(accel_vals[4]);
         ang_vel_double[5] = (double)(accel_vals[6]);
 
-        orient_cov.addData(gyro_double)
+        orient_cov.addData(gyro_double);
         lin_accel_cov.addData(lin_accel_double);
         ang_vel_cov.addData(lin_accel_double);
 
@@ -63,24 +66,30 @@ double[][] covCalculator() {
     Eigen::Matrix<double, 3, 3> lin_accel_cov_mat = lin_accel_cov.getCovariance();
     Eigen::Matrix<double, 3, 3> ang_vel_cov_mat = ang_vel_cov.getCovariance();
 
-    double covMats[3][9];
+    //double covMats[3][9];
 
-    covMats[0] = {orient_cov_mat(0),orient_cov_mat(1),orient_cov_mat(2),
+    double covMats0[] = {orient_cov_mat(0),orient_cov_mat(1),orient_cov_mat(2),
                   orient_cov_mat(3),orient_cov_mat(4),orient_cov_mat(5),
                   orient_cov_mat(6),orient_cov_mat(7),orient_cov_mat(8)};
 
-    covMats[1] = {lin_accel_cov_mat(0),lin_accel_cov_mat(1),lin_accel_cov_mat(2),
+    double covMats1[] = {lin_accel_cov_mat(0),lin_accel_cov_mat(1),lin_accel_cov_mat(2),
                   lin_accel_cov_mat(3),lin_accel_cov_mat(4),lin_accel_cov_mat(5),
                   lin_accel_cov_mat(6),lin_accel_cov_mat(7),lin_accel_cov_mat(8)};
 
-    covMats[2] = {ang_vel_cov_mat(0),ang_vel_cov_mat(1),ang_vel_cov_mat(2),
+    double covMats2[] = {ang_vel_cov_mat(0),ang_vel_cov_mat(1),ang_vel_cov_mat(2),
                   ang_vel_cov_mat(3),ang_vel_cov_mat(4),ang_vel_cov_mat(5),
                   ang_vel_cov_mat(6),ang_vel_cov_mat(7),ang_vel_cov_mat(8)};
-
-    return covMats;
+    double covMats[3][9] = {covMats0, covMats1, covMats2};
+    //covMats[0] = covMats0;
+    //covMats[1] = covMats1;
+    //covMats[2] = covMats2;
+    inputMat = covMats;
 }
 
 int main(){
-  double covMats[3][9] = covCalculator();
-  //stuff
+  double covMats[3][9];
+  covCalculator(covMats);
+  std::cout << covMats[0] << std::endl;
+  std::cout << covMats[1] << std::endl;
+  std::cout << covMats[2] << std::endl;
 }
